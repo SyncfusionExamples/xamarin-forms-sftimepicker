d3ce7dada5b6ae7293c5627b98ee2b53eb5bbbca
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 18:46:00 +0530

    Time picker sample has been added

---
+++ TimePickerSample/TimePickerSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimePickerSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickerButton_Clicked(object sender, System.EventArgs e)
+        {
+            timePicker.IsOpen = true;
+        }
+    }
+}
